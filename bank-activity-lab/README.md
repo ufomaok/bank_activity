4098424f8e0c8917e66b887420fefae7d6f6aa23
ufomaok <ufomaok@gmail.com>
2023-10-26 22:47:45 -0700

    edited README file

--- bank-activity-lab/README.md
+++ bank-activity-lab/README.md
@@ -1,23 +1,21 @@
 
 ## Project Intro/Objective
-The purpose of this project is ________. (Describe the main goals of the project and potential civic impact. Limit to a short paragraph, 3-6 Sentences)
+This project utilizes machine learning to engineer pipeline to analyze stock data to support sentiment prediction of stock prices. The project puts the coder into the shoes of a hypothetical data engineer working for a national consumer bank. The goal is to analyze terabytes of stock data to integrate with news headline data to measure how well sentiment analysis can predit stock price patterns. Creating data pipelines and utilizing object-oriented programming (OOP) principles, statistics, and data validation tools contributes to our ability to make data-driven decisions in the real world (in this example, the financial market), which can improve the services we are currently utilizing. 
 
 ### Methods Used
 * Inferential Statistics
 * Machine Learning
-* Data Visualization
-* Predictive Modeling
-* etc.
+* Unit Testing
+* Object Oriented Programming
+* Data Validation
+
 
 ### Technologies
-* R 
 * Python
-* D3
-* PostGres, MySql
-* Pandas, jupyter
-* HTML
-* JavaScript
-* etc. 
+* Python Statistics Module
+
 
 ## Project Description
-(Provide more detailed overview of the project.  Talk a bit about your data sources and what questions and hypothesis you are exploring. What specific data analysis/visualization and modelling work are you using to solve the problem? What blockers and challenges are you facing?  Feel free to number or bullet point things here)
+This project involves utilizing machine learning to determine the effectiveness of a stock dataset (spanning over nine weeks). The goal of project is to use data to analyze stock prices, to then predict stock price changes based on news sentiment. To accomplish this feat, this project utilizes OOP principles to create a class ('StockMetrics') that calculates the mean, median, and standard deviation for values. Data validation played a vital role in this project, ensuring that strings were converted into floats/missing data points didn't affect data. 
+
+There are some challenges to consider when  with working with large datasets one of them being the size of dataset. Working with terabytes can be challenging, as it will require machinery and programs that can handle large files. Additionally, it is vital to consistently test data to ensure its accuracy and integrity, as it can be easy to make mistakes (e.g.: when skipping over null values, or converting strings to numbers).
--- bank-activity-lab/code/StockMetrics.py
+++ bank-activity-lab/code/StockMetrics.py
@@ -1,4 +1,3 @@
-
 import statistics as stats
 from code.StockData import StockData
 
@@ -7,34 +6,38 @@ from code.StockData import StockData
 
 class StockMetrics(StockData):
     def __init__(self, path):
-        # call the parent method's constructor
+        #Call  parent method's constructor
         super(StockMetrics, self).__init__(path)
 
-        # now that we've ran self.load(), we can interact with "self.data" as a
-        # list of lists
+        #Running self.load to interact with self.data
         self.load()
-
+    
     def average01(self):
         averages = []
 
-        #iterate over each row of data
-        #for loop will go through each row
-        #take the row and save it as a row
-
+        #Iterating over each row of data
+        #For loop iterating through each row
         for row in self.data:
             data_row = row[1:]
 
+            #Creating new variable y, to store list of float values for all rows (excluding header)
             y = []
+
+            #For loop iterating through each row in new saved values (data_row)
+            #Using append to store floats for all rows in the y variable
+
             for x in data_row:
                 try:
                     y.append(float(x))
+
+                #Except Value Error will skip over invalid values
                 except ValueError:
                     continue
 
-            #use stats package to find mean of y
+            #Created new variable to store mean of y variable list values
             newaverage = stats.mean(y)
             
-            #now append list of average AND round to three decimals
+            #Appending mean values (rounded to three decimals) to averages variables
             rounded_average = round(newaverage, 3)
             averages.append(rounded_average)
 
@@ -43,20 +46,29 @@ class StockMetrics(StockData):
 
     def median02(self):
         medians = []
-        
-       
+
+        #Iterating over each row of data
+        #For loop iterating through each row           
         for row in self.data:
             data_row2 = row[1:]
 
+            #Creating new variable y2, to store list of float values for all rows (excluding header)
             y2 = []
+
+            #For loop iterating through each row in new saved values (data_row2)
+            #Using append to store floats for all rows in the y variable
             for x in data_row2:
                 try:
                     y2.append(float(x))
+
+                #Except Value Error will skip over invalid values
                 except ValueError:
                     continue
 
+            #Created new variable to store mean of y2 variable list values
             newmedian = stats.median(y2)
 
+            #Appending median values into medians variable
             medians.append(newmedian)
 
         return medians
@@ -65,19 +77,28 @@ class StockMetrics(StockData):
     def stddev03(self):
         sdeviation = []
 
+        #Iterating over each row of data
+        #For loop iterating through each row 
         for row in self.data:
             data_row3 = row[1:]
 
+            #Creating new variable y3, to store list of float values for all rows (excluding header)
             y3 = []
             for x in data_row3:
                 try: 
                     y3.append(float(x))
+
+                #Except Value Error will skip over invalid values
                 except ValueError:
                     continue
 
+            #Created new variable to store standard deviation of y3 variable list values
             new_sd = stats.stdev(y3)
 
+            #Created new variable to rounded values (by three decimals)
             new_sd2 = round(new_sd, 3)
+
+            #Appending rounded values into sdeviation variable
             sdeviation.append(new_sd2)
 
         return sdeviation
